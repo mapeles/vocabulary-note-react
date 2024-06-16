45355c4e37e5ce595ff03b6806ccd73525d6fe74
mapeles <tty6421@gmail.com>
2024-06-16 17:35:14 +0900

    문장 보기 추가

--- src/components/SentenceTab/SentencePage.jsx
+++ src/components/SentenceTab/SentencePage.jsx
@@ -1,21 +1,45 @@
-import { Route, Routes, useNavigate } from "react-router-dom";
+import { Route, Routes, useLocation, useNavigate } from "react-router-dom";
 import TitleName from "../ui/Title";
 import Top from "../ui/Top";
 import { Detail, Word, WordText } from "../VocaTab/VocaElement";
+import styled from "styled-components";
+import { useState } from "react";
 
 const SentenceDB={
     "66736108-c769-42da-b0c6-64ef05e7b34f":{
         SentenceName:"테스트용 문장",
         contents:{
-            Sentence:["이것은",,"테스트용","문장입니다.","리엑트"],
+            Sentence:["이것은","테스트용","문장입니다.","리엑트"],
             blind:[true,false,false,true]
         }
     }
 }
+function WordElementComp(props){
+    const [blind, setBlind] = useState(props.blind)
+    return(
+        <WordElement blind={blind} onClick={() => setBlind(!blind)}>{props.contents}</WordElement>
+    )
+}
+const WordElement = styled.p`
+    &:hover {cursor: pointer;}
+    user-select: none;
+    display: inline;
+    background-color: ${(props) => (
+    props.blind ?
+     `grey` : 
+     `null`
+    )};
+    color: ${(props) => (
+    props.blind ?
+     `grey` : 
+     `white`
+    )};
+`
 function SentenceElement(props) {
-    const keyValue = props.keyValue
+    const id = props.keyValue
+    const navigate = useNavigate();
     return (
-        <div onClick={() => {}}>
+        <div onClick={() => navigate("Detail/", {state:id})}>
             <Word>
                 <Detail>
                     <WordText>{props.SentenceName}</WordText>
@@ -29,7 +53,32 @@ export function SentencePage(){
         <div style={{height:'100%'}}>
             <Routes>
                 <Route path="/" element={<Sentence/>}/>
+                <Route path="/Detail" element={<SentenceDetail/>}/>
             </Routes>
+        </div>
+    )
+}
+function SentenceDetail(){
+    const location = useLocation()
+    const id = location.state
+    const navigate = useNavigate();
+    console.log(SentenceDB[id].SentenceName)
+    return(
+        <div style={{color:"white", marginTop:'1vh'}}>
+            <Top>
+                <TitleName onClick={() => navigate('/Sentence')}>⬅️</TitleName>
+                <TitleName>{SentenceDB[id].SentenceName}</TitleName>
+                <div style={{color:'white'}} onClick={() => {}}>
+                ✐
+                </div>
+            </Top>
+            {Object.keys(SentenceDB[id].contents.blind).map(key => (
+                <div style={{display:"inline"}}>
+                    <WordElementComp blind={SentenceDB[id].contents.blind[key]} contents={SentenceDB[id].contents.Sentence[key]}/>
+                    <p style={{display:"inline",userSelect: 'none'}}> </p>
+                </div>
+            ))}
+
         </div>
     )
 }
